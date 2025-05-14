aa651378a2568e701976bc92a5d9e8d581ea34d1
thibaut <perry@et.esiea.fr>
2025-05-14 11:30:03 +0200

    correction register

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -1,10 +1,9 @@
 <?php
 
-// src/Controller/ProfileController.php
 namespace App\Controller;
 
 use App\Entity\User;
-use App\Form\ProfileFormType;
+use App\Form\RegistrationForm;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -14,35 +13,38 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class RegistrationController extends AbstractController
 {
-    #[Route('/register', name: 'app_register')]
-    public function edit(
+    #[Route(path: '/register', name: 'app_register')]
+    public function register(
         Request $request,
-        EntityManagerInterface $em,
-        UserPasswordHasherInterface $passwordHasher
+        UserPasswordHasherInterface $passwordHasher,
+        EntityManagerInterface $em
     ): Response {
-        /** @var User $user */
-        $user = $this->getUser();
+        // 1) on crée un nouvel User
+        $user = new User();
+        $form = $this->createForm(RegistrationForm::class, $user);
 
-        $form = $this->createForm(ProfileFormType::class, $user);
+        // 2) on hydrate avec les données POST
         $form->handleRequest($request);
-
         if ($form->isSubmitted() && $form->isValid()) {
-            // Si un nouveau mot de passe a été saisi
-            $newPwd = $form->get('newPassword')->getData();
-            if ($newPwd) {
-                $hashed = $passwordHasher->hashPassword($user, $newPwd);
-                $user->setPassword($hashed);
-            }
+            
+            $user->setCreatedAt(new \DateTimeImmutable());
+            // 3) hash du mot de passe
+            $plainPwd = $form->get('plainPassword')->getData();
+            $hashedPwd = $passwordHasher->hashPassword($user, $plainPwd);
+            $user->setPassword($hashedPwd);
 
+            // 4) on persiste et flush
             $em->persist($user);
             $em->flush();
 
-            $this->addFlash('success', 'Profil mis à jour avec succès.');
-            return $this->redirectToRoute('app_profile');
+            // 5) on redirige vers la page de login
+            $this->addFlash('success', 'Votre compte a bien été créé ! Vous pouvez maintenant vous connecter.');
+            return $this->redirectToRoute('app_login');
         }
 
-        return $this->render('profile/edit.html.twig', [
-            'profileForm' => $form->createView(),
+        // 6) on affiche le formulaire
+        return $this->render('registration/register.html.twig', [
+            'registrationForm' => $form->createView(),
         ]);
     }
 }
